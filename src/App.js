6b600263addae8cc76482db1ac6934dc221d3857
berkayozay <berkayyozayy@gmail.com>
2021-04-20 11:29:43 +0300

    app body sections

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Mail from './components/Mail';
+import EmailList from './components/EmailList';
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Sidebar />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className='app__body'>
+          <Sidebar />
+          <Switch>
+            <Route path='/mail'>
+              <Mail />
+            </Route>
+            <Route path='/'>
+              <EmailList />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
