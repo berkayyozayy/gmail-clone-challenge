08c3eceb570fcb5d45fd4141134dcf3047f003d3
berkayozay <berkayyozayy@gmail.com>
2021-04-21 12:27:07 +0300

    check persisting user

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
@@ -7,15 +7,27 @@ import Mail from './components/Mail';
 import EmailList from './components/EmailList';
 import SendMail from './components/SendMail';
 import { selectSendMessageIsOpen } from './features/mailSlice';
-import { useSelector } from 'react-redux'
-import { selectUser } from './features/userSlice';
+import { useDispatch, useSelector } from 'react-redux'
+import { selectUser, login } from './features/userSlice';
 import Login from './components/Login';
-
+import { auth } from './db/Firebase'
 
 function App() {
-
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen)
   const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        dispatch(login({
+          displayName: user.displayName,
+          email: user.email,
+          photoUrl: user.photoURL
+        }))
+      }
+    })
+  }, [])
 
   return (
     <Router>
