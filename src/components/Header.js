29a17a5b8d86b423968985ddb27a49d56b97de16
berkayozay <berkayyozayy@gmail.com>
2021-04-21 13:01:52 +0300

    display avatar

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,11 @@ import SearchIcon from '@material-ui/icons/Search';
 import { ArrowDropDown } from '@material-ui/icons';
 import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
 
 function Header() {
+    const user = useSelector(selectUser)
     return (
         <div className='header'>
             <div className='header__left'>
@@ -33,7 +36,7 @@ function Header() {
                 <IconButton>
                     <NotificationsIcon />
                 </IconButton>
-                <Avatar />
+                <Avatar src={user?.photoUrl} />
             </div>
         </div>
     )
